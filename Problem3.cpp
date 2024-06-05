b3e5a53ba838a655c9c74f7642e7553c23e723c9
Mateusz Ż <51126753+godryk75@users.noreply.github.com>
2024-06-05 16:06:19 +0200

    Add files via upload
    dodano tworzenie punktów i energii swietlnej, na potrzeby testow, Finalnie punkty beda pobierane z otoczki wypuklej

--- Problem3.cpp
+++ Problem3.cpp
@@ -13,40 +13,57 @@ struct Punkty{
     int y;
 };
 
-void Najsilniejsi_straznicy(vector<Plaszczaki> straznicy){
+void wybierz_najsilniejszych(vector<Plaszczaki> &straznicy){
     ///sortujemy wzgledem energii
     sort(straznicy.begin(), straznicy.end(), [](const Plaszczaki &a, const Plaszczaki &b) {
         return a.energia > b.energia;
     });
-    for(int i = 0; i<=7; i++){
+    /*for(int i = 0; i < 7; i++){
         cout << straznicy[i].numer << " " << straznicy[i].energia << endl;
-    }
+    } */
 }
+
+
 
 int main(){
 ///generator liczb losowych
 srand(time(nullptr));
 
-///kontener na straznikow
+///kontenery na straznikow i punkty
 vector<Plaszczaki> straznicy;
+vector<Punkty> punkty;
 
-///inicjalizujemy straznika
 Plaszczaki straznik;
+Punkty punkt;
 
 ///losujemy energie straznikom
-for(int i = 0; i < 10; i++) {
+for(int i = 0; i < 100; i++) {
         straznik.numer = i;
-        straznik.energia = rand() % 5 + 1;
+        straznik.energia = rand() % 100 + 1;
         straznik.predkosc = rand() % 10 + 1;
         straznicy.push_back(straznik);
     }
 
+///losujemy koordynaty i swiatlosc punktow
+for(int i = 0; i < 30; i++) {
+        punkt.x = rand() % 50 + 0;
+        punkt.y = rand() % 50 + 0;
+        punkt.swiatlosc = rand() % 100 + 1;
+        punkty.push_back(punkt);
+    }
 
-///wypisywanie straznikow i ich mocy
-/*for(int i = 0; i< 10; i++){
-    cout << "Numer straznika: " <<straznicy[i].numer << " Energia: " << straznicy[i].energia << " Predkosc:" << straznicy[i].predkosc << endl;
-} */
 
+
+/*wybierz_najsilniejszych(straznicy);
+    for(int i = 0; i < 7; i++){
+        cout << "Numer: " << straznicy[i].numer << " Energia: " << straznicy[i].energia << endl;
+    }
+*/
+
+///wyswietlanie punktow
+for(int i = 0; i < 30; i++){
+    cout << "x: "  <<punkty[i].x << " y: "<< punkty[i].y << " swiatlosc: "<<punkty[i].swiatlosc << endl;
+}
 
 
 return 0;
